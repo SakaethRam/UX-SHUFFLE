a27e5dc45904d8e6b2360b2ec36a5344c4ada821
R Sakaeth Ram <sakaethrambusiness@gmail.com>
2026-09-15 03:34:38 +0530

    Update Gemini.js

--- Meta/src/Config/Gemini.js
+++ Meta/src/Config/Gemini.js
@@ -4,7 +4,7 @@ import {
     HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyD8d8aiNMAFdtlIQCcLcyiijCTSevNBEr0";
+const apiKey = "<API_KEY>";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
